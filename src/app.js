e8759674427e793394ffc99a35c777953f1c7caf
Prince Kumar <princeuiet123@gmail.com>
2026-02-26 03:49:52 +0530

    fixing CORS issue

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://exuverse-assignment.vercel.app/",
+    origin: "https://exuverse-assignment.vercel.app",
     credentials: true,
   })
 );
