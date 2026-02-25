ef02d73aad8f4182dc05e050679354f78189fb0f
Prince Kumar <princeuiet123@gmail.com>
2026-02-25 23:01:21 +0530

    signup and login apis done

---
+++ src/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require("cookie-parser")
+const connectDB = require("./config/database")
+const cors = require("cors")
+const app = express();
+
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser())
+
+const authRouter = require("./routes/auth")
+
+app.use("/", authRouter)
+
+connectDB()
+.then(() => {
+    console.log("Database connection established...")
+    app.listen(5000, () => {
+    console.log("server up")
+})
+})
+.catch((err) => {
+    console.error("Database cannot be connected" + err.message)
+})
---
+++ src/routes/auth.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const authRouter = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const { userAuth } = require("../middlewares/auth");
+const { SignUpValidation } = require("../utils/validations");
+
+authRouter.post("/signup", async (req, res) => {
+  try {
+    SignUpValidation(req)
+    const { firstName, lastName, emailId, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+    res.cookie("token", token);
+    res.json({ message: "User signed up successfully", data: savedUser });
+  } catch (err) {
+    res.status(401).send(`Error: ${err.message}`);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const isPassword = await user.validatePassword(password);
+    if (isPassword) {
+      const token = await user.getJWT();
+      res.cookie("token", token);
+      res.json({ message: "User logged in successfully", data: user });
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(401).send(`Error: ${err.message}`);
+  }
+});
+
+
+
+authRouter.get("/auth/me", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      throw new Error("User not found");
+    }
+    res.json({ message: "User authenticated", data: user });
+  } catch (err) {
+    res.status(401).send(`Error: ${err.message}`);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send("Logged out");
+});
+
+module.exports = authRouter;
